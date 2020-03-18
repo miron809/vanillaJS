2737acfbbc8c653166ab37c2e4a79ce60e3cc54c
Dmitriy Mironenko <dim@uain.su>
2020-03-18 17:50:18 +0200

    added RxJs to project / changed infinite-scroll

--- projects/infinite-scroll/script.js
+++ projects/infinite-scroll/script.js
@@ -1,50 +1,49 @@
+const {from, fromEvent, of} = rxjs;
+const {ajax} = rxjs.ajax;
+const {concatMap, delay, debounceTime} = rxjs.operators;
+
 const postsContainer = document.getElementById('posts-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
 let limit = 5;
 let page = 1;
-
-// Fetch posts from API
-async function getPosts() {
-    const res = await fetch(
-        `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
-    );
-
-    return data = await res.json();
-}
+let processing = false;
 
 // Show posts in DOM
-async function showPosts() {
-    const posts = await getPosts();
+function showPosts() {
+    processing = true;
+    const getPosts$ = ajax.getJSON(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
+    getPosts$
+        .pipe(
+            concatMap(item => from(item)
+                .pipe(
+                    concatMap(post => {
+                        const postEl = createPostElement(post);
+                        return of(postEl).pipe(delay(300));
+                    })
+                ))
+        )
+        .subscribe(postEl => {
+            postsContainer.appendChild(postEl);
+            postEl.classList.add('post-show');
+            loading.classList.remove('show');
+            processing = false;
+        });
+    page++;
+}
 
-    posts.forEach(post => {
-        const postEl = document.createElement('div');
-        postEl.classList.add('post');
-        postEl.innerHTML = `
+function createPostElement(post) {
+    const postEl = document.createElement('div');
+    postEl.classList.add('post');
+    postEl.innerHTML = `
       <div class="number">${post.id}</div>
       <div class="post-info">
         <h2 class="post-title">${post.title}</h2>
         <p class="post-body">${post.body}</p>
       </div>
     `;
-
-        postsContainer.appendChild(postEl);
-    });
-}
-
-// Show loader & fetch more posts
-function showLoading() {
-    loading.classList.add('show');
-
-    setTimeout(() => {
-        loading.classList.remove('show');
-
-        setTimeout(() => {
-            page++;
-            showPosts();
-        }, 100);
-    }, 1000);
+    return postEl;
 }
 
 // Filter posts by input
@@ -67,12 +66,17 @@ function filterPosts(e) {
 // Show initial posts
 showPosts();
 
-window.addEventListener('scroll', () => {
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-
-    if (scrollTop + clientHeight >= scrollHeight - 5) {
-        showLoading();
-    }
-});
+const scrollToBottom$ = fromEvent(window, 'scroll');
+scrollToBottom$
+    .pipe(debounceTime(500))
+    .subscribe(() => {
+        const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+        if (!processing) {
+            if (scrollTop + clientHeight >= scrollHeight - 5) {
+                showPosts();
+                loading.classList.add('show')
+            }
+        }
+    });
 
 filter.addEventListener('input', filterPosts);
